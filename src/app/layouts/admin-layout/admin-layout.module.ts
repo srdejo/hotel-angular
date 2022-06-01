2f992a6b1b5ccaf4652fafb24200fa19dbb7daa6
Daniel Jimenez <srdejo@gmail.com>
2022-06-01 17:55:41 -0500

    core-feature-shared

--- src/app/layouts/admin-layout/admin-layout.module.ts
+++ src/app/layouts/admin-layout/admin-layout.module.ts
@@ -4,12 +4,12 @@ import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AdminLayoutRoutes } from './admin-layout.routing';
 import { DashboardComponent } from '../../dashboard/dashboard.component';
-import { UserProfileComponent } from '../../user-profile/user-profile.component';
-import { TableListComponent } from '../../table-list/table-list.component';
-import { TypographyComponent } from '../../typography/typography.component';
-import { IconsComponent } from '../../icons/icons.component';
-import { MapsComponent } from '../../maps/maps.component';
-import { NotificationsComponent } from '../../notifications/notifications.component';
+import { UserProfileComponent } from '../../feature/user-profile/user-profile.component';
+import { TableListComponent } from '../../feature/table-list/table-list.component';
+import { TypographyComponent } from '../../feature/typography/typography.component';
+import { IconsComponent } from '../../feature/icons/icons.component';
+import { MapsComponent } from '../../feature/maps/maps.component';
+import { NotificationsComponent } from '../../feature/notifications/notifications.component';
 
 import {MatButtonModule} from '@angular/material/button';
 import {MatInputModule} from '@angular/material/input';
--- src/app/layouts/admin-layout/admin-layout.routing.ts
+++ src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -1,12 +1,12 @@
 import { Routes } from '@angular/router';
 
 import { DashboardComponent } from '../../dashboard/dashboard.component';
-import { UserProfileComponent } from '../../user-profile/user-profile.component';
-import { TableListComponent } from '../../table-list/table-list.component';
-import { TypographyComponent } from '../../typography/typography.component';
-import { IconsComponent } from '../../icons/icons.component';
-import { MapsComponent } from '../../maps/maps.component';
-import { NotificationsComponent } from '../../notifications/notifications.component';
+import { UserProfileComponent } from '../../feature/user-profile/user-profile.component';
+import { TableListComponent } from '../../feature/table-list/table-list.component';
+import { TypographyComponent } from '../../feature/typography/typography.component';
+import { IconsComponent } from '../../feature/icons/icons.component';
+import { MapsComponent } from '../../feature/maps/maps.component';
+import { NotificationsComponent } from '../../feature/notifications/notifications.component';
 
 export const AdminLayoutRoutes: Routes = [
     // {
