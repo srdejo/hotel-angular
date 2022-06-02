c73491091b9b98b83b29e923b57826e865032393
Daniel Jimenez <srdejo@gmail.com>
2022-06-02 12:02:30 -0500

    Remove unused components typography

--- src/app/feature/home/home-routing.module.ts
+++ src/app/feature/home/home-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { InicioComponent } from './inicio/inicio.component';
+import { DashboardComponent } from './inicio/dashboard.component';
+import { InicioComponent } from '../dashboard/inicio.component';
 
 const routes: Routes = [{
   path: '',
--- src/app/feature/home/home.module.ts
+++ src/app/feature/home/home.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
-import { InicioComponent } from './inicio/inicio.component';
+import { InicioComponent } from '../dashboard/inicio.component';
 import { CoreModule } from 'app/core/core.module';
 
 
